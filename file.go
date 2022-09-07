4996365d3a1d5d9a90884d4af1082739137b9a62
ChenYong <chenyong1@ppmoney.com>
2022-09-07 17:45:10 +0800

    feat: file 增加判断路径是否存在方法

---
+++ file.go
@@ -0,0 +1,16 @@
+package langs
+
+import "os"
+
+// PathExists 判断所给的文件路径path是否存在
+func PathExists(path string) (bool, error) {
+    _, err := os.Stat(path)
+    if err == nil {
+        return true, nil
+    }
+
+    if os.IsNotExist(err) {
+        return false, nil
+    }
+    return false, err
+}
